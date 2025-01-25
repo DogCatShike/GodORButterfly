7a0731bcb877cf9bda58b307a7d371e733d497dc
DogCat_Shike <dcsk.flzj@gmail.com>
2025-01-25 15:55:13 +0800

    <feature>add: 立项

---
+++ Assets/Src_Runtime/Main.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GB
+{
+    public class Main : MonoBehaviour
+    {
+        void Awake()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
