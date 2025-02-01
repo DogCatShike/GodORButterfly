1e5532c8a7e9a39bb1ed3d5f5c6ae6b3a5190d3e
Lazyihui <chengbaozi6@qq.com>
2025-02-01 17:42:39 +0800

    <feature>add: Panel_Bag Panel_BagELement .prefab

---
+++ Assets/Src_Runtime/App_UI/Panel/Bag/Panel_Bag.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+
+namespace GB {
+
+    public class Panel_Bag : MonoBehaviour {
+
+        
+    }
+}
---
+++ Assets/Src_Runtime/App_UI/Panel/Bag/Panel_BagElement.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace GB {
+
+    public class Panel_BagElement : MonoBehaviour {
+
+
+    }
+}
